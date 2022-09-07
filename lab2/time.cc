5691bbe5652bd05e774b1ebe46aad18346d69b99
Weixiong <weizh994@student.liu.se>
2022-09-07 17:01:07 +0200

    Declare some function and add two necessary files

--- lab2/time.cc
+++ lab2/time.cc
@@ -1,11 +1,25 @@
 #include<iostream>
 using namespace std;
+bool is_valid();
+string to_string();
+bool is_am();
+time operator+(time const& t,int n);//Do we need n+t?
+time operator-(time const& t,int n);//Do we need n-t?
+time& operator++(time& t);
+time operator--(time& t,int);
+bool operator<(time const& t1,time const& t2);
+bool operator>(time const& t1,time const& t2);
+bool operator<=(time const& t1,time const& t2);
+bool operator>=(time const& t1,time const& t2);
+bool operator==(time const& t1,time const& t2);
+bool operator!=(time const& t1,time const& t2);
+
 struct time
 {
 	int hour;
 	int minute;
 	int second;
-}
+};
 
 time& operator>>(istream& is, time& t)
 {
