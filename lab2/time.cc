d248e4b2a2844abdf1de24cd693883f3e7daace6
Weixiong <weizh994@student.liu.se>
2022-09-09 00:00:39 +0200

    Improve the input function, but it should use iteration

--- lab2/time.cc
+++ lab2/time.cc
@@ -16,7 +16,7 @@ bool operator>=(time const& t1,time const& t2);
 bool operator==(time const& t1,time const& t2);
 bool operator!=(time const& t1,time const& t2);
 time& operator>>(istream& is,time& t);
-
+void error();
 struct time
 {
 	int hour;
@@ -24,8 +24,51 @@ struct time
 	int second;
 };
 
-time& operator>>(istream& is, time& t)
+time& operator>>(istream& is, time& t)//should use iteration!
 {
-    is>>t.hour>>t.minute>>t.second;
+    //is>>t.hour>>t.minute>>t.second;
+    while(is>>t.hour){
+        if(is.bad()||is.eof()||is.fail()){
+            is.clear();
+            is.ignore(999999,'\n');
+            error();
+        }
+        else if((t.hour > 23) || (t.hour < 0)){
+            error();
+        }
+        else{
+            break;
+        }
+    }
+    while(is>>t.minute){
+        if(is.bad()||is.eof()||is.fail()){
+            is.clear();
+            is.ignore(999999,'\n');
+            error();
+        }
+        else if((t.minute > 59) || (t.minute < 0)){
+            error();
+        }
+        else{
+            break;
+        }
+    }
+    while(is>>t.second){
+        if(is.bad()||is.eof()||is.fail()){
+            is.clear();
+            is.ignore(999999,'\n');
+            error();
+        }
+        else if((t.second > 59) || (t.second < 0)){
+            error();
+        }
+        else{
+            break;
+        }
+    }
     return is;
+}
+
+void error(){
+    cerr << "ERROR: Illegal input!"<<endl;
 }
