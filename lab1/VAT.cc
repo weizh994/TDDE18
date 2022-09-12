0d6ba04c43c1b4f42406327db256cd5eca311dbb
Edwin <edwch861@student.liu.se>
2022-09-12 15:45:44 +0200

    changes made with complementary work

--- lab1/VAT.cc
+++ lab1/VAT.cc
@@ -5,23 +5,11 @@ using namespace std;
 
 // TODO: Complementary work: Seperate the different parts of the
 // code with an empty line.
-// CHECKED
-// TODO: Complementary work: Have spaces between operators to
-// increase the codes readability
-// CHECKED
 // TODO: Complementary work: Don't repeat characters but use 
-// manipulators from iomanip (example setfill())
-// CHECKED
-// TODO: Complementary work: Doesn't work for the following input: 
-// first price = 9990 
-// last price = 10000 
-// stide = 0.1 
-// tax = 20 
-// Specifically it doesn't print the final row. 
-// Hint: Do you know how many iterations needs to be done? 
-// If so, is there a better type of loop you can us? 
-// CHECKED
-int main(){
+// manipulators from iomanip (example setprecision())
+
+int main()
+{
 	cout << "INPUT PART\n"
 			 << setfill('=') << setw(10) << "\0"
 			 << endl;
@@ -31,33 +19,43 @@ int main(){
 	float first_price;
 	cin >> first_price;
 	cin.ignore(9999999,'\n');
-	while(first_price<0.0){
+	
+	
+ 	while(first_price<0.0)
+	{
 		cerr << "ERROR: First price must be at least 0 (zero) SEK" << endl;
 		cout << left << setw(input_lenth) << "Enter first price" << ": ";
 		cin >> first_price;
 		cin.ignore(9999999,'\n');
 	}
+	
 	cout << left << setw(input_lenth) << "Enter last prise" << ": ";
 	float last_price;
 	cin >> last_price;
 	cin.ignore(9999999,'\n');
-	while(last_price<first_price){
+	
+	while(last_price<first_price)
+	{
 		cerr << "ERROR: Last price must be at least "
 				 << fixed << setprecision(2) << first_price << " SEK" << endl;
 		cout << left << setw(input_lenth) << "Enter last prise" << ": ";
 		cin >> last_price;
 		cin.ignore(9999999,'\n');
 	}
+	
 	cout << left << setw(input_lenth) << "Enter stride" << ": ";
 	float stride;
 	cin >> stride;
 	cin.ignore(9999999,'\n');
-	while(stride < 0.01){
+	
+	while(stride < 0.01)
+	{
 		cerr << "ERROR: Stride must be at least 0.01" << endl;
 		cout << left << setw(input_lenth) << "Enter stride" << " :";
 		cin >> stride;
 		cin.ignore(9999999,'\n');
 	}
+	
 	cout << left << setw(input_lenth) << "Enter tax percent" << ": ";
 	float tax_percent;
 	cin >> tax_percent;
@@ -77,7 +75,9 @@ int main(){
 	float price = first_price;
 	int row;
 	row = static_cast<int>((last_price-first_price)/stride);
-	for(int i = 0; i <= row; ++i){
+	
+	for(int i = 0; i <= row; ++i)
+	{
 		price = stride * i + first_price;
 		cout << right << setfill(' ') << setw(12) << fixed << setprecision(2) << price;
 		float tax = price*tax_percent/100.00;
--- lab1/file.cc
+++ lab1/file.cc
@@ -3,18 +3,11 @@
 #include<fstream>
 using namespace std;
 
-// TODO: Complementary work: You must print a warning and end the program if the file is empty. 
-// CHECKED
-// TODO: Complementary work: Declaring the shortest word as a int at first
-// is not the best way to solve this, what if a word is longer? 
-// Hint: Can you use the first word in the file?
-// CHECKED
-// TODO: Complementary work: Functions and blocks need to be 
-// indented properly (be consistent). 
-// CHECKED
-// TODO: Complementary work: Float is not the best variable to use, rounding errors are more
-// prone to happen.
-// Double?
+// TODO: Complementary work: The average word should give two decimals
+// TODO: Complementary work: Seperate the different parts of the
+// code with an empty line.
+// TODO: Complementary work: Use {} for if and end statements for 
+// readability if you have it around some.
 
 int main(){		
 	ifstream read_file;
@@ -24,36 +17,53 @@ int main(){
 	double sum = 0.0;
 	string shortest_word,longest_word;
 	long shortest_word_length,longest_word_length;
-	if(read_file >> word){//first word
+	
+	
+	if(read_file >> word)//first word
+	{
 		shortest_word_length = word.size();
 		longest_word_length = word.size();
 		num++;
 	}
-	while(read_file >> word){
+	while(read_file >> word)
+	{
 		num++;
 		sum+=word.size();
-		if(word.size() < shortest){
+		if(word.size() < shortest)
+		{
 			shortest_word_length=word.size();
 			shortest_word = word;
 		}
-		if(word.size() > longest_word_length){
+		if(word.size() > longest_word_length)
+		{
 			longest_word_length=word.size();
 			longest_word = word;
 		}
 	}
 	read_file.close();
-	if(num > 1)cout << "There are " << num << " words in the file." << endl;
-	else cout << "There is " << num << " word in the file." << endl;
-	if(num!=0){
+	
+	
+	if(num > 1)
+	{
+		cout << "There are " << num << " words in the file." << endl;
+	}
+	else
+	{
+		cout << "There is " << num << " word in the file." << endl;
+	}
+	
+	if(num!=0)
+	{
 		cout << "The shortest word was \""
 				 << shortest_word << "\" with "
 				 << shortest_word_length << " character(s)." << endl;
 		cout << "The longest word was \""
 				 << longest_word << "\" with "
 				 << longest_word_length << " character(s)." << endl;
-		cout << "The average length was "
+		cout << "The average length was "<< fixed << setprecision(2)
 				 << sum/num << " character(s)." << endl;
 	}
 	else cerr << "WARRING: The file is empty!" << endl;
+	
 	return 0;
 }
