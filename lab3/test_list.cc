4385331cf70b140c61cf9339a96185db42a55f5f
Weixiong <weizh994@student.liu.se>
2022-11-02 12:47:09 +0100

    modify test file without print

--- lab3/test_list.cc
+++ lab3/test_list.cc
@@ -33,38 +33,32 @@ TEST_CASE("Add some value")
   REQUIRE(l.is_empty() == true);
   REQUIRE(l.size() == 0);
   l.insert(5);
+  CHECK_FALSE(l.is_empty);
+  CHECK(l.size == 1);
+  CHECK(l.getValue(1) == 5);
   l.insert(3);
+  CHECK_FALSE(l.is_empty);
+  CHECK(l.size == 2);
+  CHECK(l.getValue(1) == 3);
+  CHECK(l.getValue(2) == 5);
   l.insert(9);
+  CHECK_FALSE(l.is_empty);
+  CHECK(l.size == 3);
+  CHECK(l.getValue(1) == 3);
+  CHECK(l.getValue(2) == 5);
+  CHECK(l.getValue(3) == 9);
   l.insert(7);
-  l.print(); // 3,5,7,9
-  l.insert(1);
-  l.print(); // 1,3,5,7,9
-  l.insert(1);
-  l.print(); // 1,1,3,5,7,9
-  l.insert(8);
-  l.print(); // 1,1,3,5,7,8,9
-  l.insert(11);
-  l.print(); // 1,1,3,5,7,8,9,11
+  CHECK_FALSE(l.is_empty);
+  CHECK(l.size == 4);
+  CHECK(l.getValue(1) == 3);
+  CHECK(l.getValue(2) == 5);
+  CHECK(l.getValue(2) == 7);
+  CHECK(l.getValue(4) == 9);
 }
 TEST_CASE("Remove some value")
 {
   List l{5, 3, 9, 7};
   REQUIRE(l.is_empty() == false);
   REQUIRE(l.size() == 4);
-  l.insert(5);
-  l.insert(3);
-  l.insert(9);
-  l.insert(7);
-  l.print();   // 3,5,7,9
-  l.remove(1); // cout: "Not exist"
-  l.print();   // 3,5,7,9
-  l.remove(5);
-  l.print(); // 3,7,9
-  l.remove(3);
-  l.print(); // 7,9
-  l.remove(7);
-  l.print(); // 9
-  l.remove(9);
-  l.print(); //"List is Empty"
 }
 // It is your job to create new test cases and fully test your Sorted_List class
