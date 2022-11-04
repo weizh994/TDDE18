6504b4a1ea535fe9497ec94576d05c94074d67e1
Weixiong <weizh994@student.liu.se>
2022-11-04 15:29:37 +0100

    create struct Connection

--- lab4/circuits.hh
+++ lab4/circuits.hh
@@ -1,6 +1,53 @@
 #ifndef CIRCUITS_H
 #define CIRCUITS_H
+#include <string>
 
+struct Connection
+{
+  double Volt = 0.0;
+  bool can_change = true;
+};
 
+class Components
+{
+protected:
+  string Name;
+  Connection *V_P;
+  Connection *V_N;
+
+public:
+  Components();
+  ~Components();
+};
+
+class Battery : public Components
+{
+private:
+  double Volt;
+
+public:
+  Battery(/* args */);
+  ~Battery();
+};
+
+class Resistor : public Components
+{
+private:
+  double Ohm;
+
+public:
+  Resistor();
+  ~Resistor();
+};
+
+class Capacitor : public Components
+{
+private:
+  double Fahrad;
+
+public:
+  Capacitor(/* args */);
+  ~Capacitor();
+};
 
 #endif
